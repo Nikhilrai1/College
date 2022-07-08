56bce3758b13c878fb6d4548342de11d25d48fa3
Nikhil Rai <nrai9027@gmail.com>
2022-07-08 09:48:48 -0700

    Third Commit

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,27 +1,63 @@
 import React from 'react';
 import Link from "next/Link";
+// import UserDropdown from './UserDropdown';
 import { AiOutlineSearch } from 'react-icons/ai';
 
 const Navbar = () => {
     return (
-        <div>
-            <header className="text-gray-600 body-font shadow-md px-7">
-                <div className="flex flex-wrap p-5 flex-col md:flex-row items-center">
-                    <span className="left text-2xl text-purple-900 font-bold w-1/6">Dashboard</span>
-                    <div className="middle flex w-4/6">
-                        <div class="flex items-center w-full px-7">
-                            <span className="p-1 border border-black"><AiOutlineSearch className="text-xl"/></span>
-                            <input placeholder="Search" type="text" id="name" name="name" className="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" />
-                        </div>
-                    </div>
-
-                    <div className="right w-1/6 flex justify-center items-center">
-                        <button className="flex text-white bg-purple-700 border-0 py-2 px-8 focus:outline-none hover:bg-purple-900 rounded text-lg">Settings</button>
-                    </div>
+        <nav
+            style={{
+                height: "75px",
+                width: "100vw",
+                position: "fixed",
+                zIndex: 100
+            }}
+            className="bg-gray-50 border-gray-200 px-2 sm:px-4 py-3 rounded dark:bg-gray-800">
+            <div className="flex flex-wrap justify-between items-center mx-auto">
+                <div className="logo">
+                    <img
+                        style={{
+                            height: "50px",
+                            width: "200px",
+                            objectFit: "contain"
+                        }}
+                        src="/logo.png"
+                        alt="logo"
+                        className='cursor-pointer'
+                    />
                 </div>
-            </header>
-        </div>
+
+                <div className="hidden flex-initial w-full md:block md:w-auto mx-2" id="mobile-menu">
+                    <ul className="flex flex-col mt-4 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium">
+                        <li>
+                            <a href="#" className="block py-2 pr-4 pl-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white" aria-current="page">Home</a>
+                        </li>
+                        <li>
+                            <a href="#" className="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">About</a>
+                        </li>
+                        <li>
+                            <a href="#" className="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Services</a>
+                        </li>
+                        <li>
+                            <a href="#" className="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Pricing</a>
+                        </li>
+                        <li>
+                            <a href="#" className="block py-2 pr-4 pl-3 text-gray-700 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Contact</a>
+                        </li>
+                    </ul>
+
+                </div>
+                <div className="inputField">
+                    < input
+                        type="text"
+                        placeholder="Search here..."
+                        className="border-0 px-3 py-3 placeholder-blueGray-300 text-blueGray-600 relative bg-white bg-white rounded text-sm shadow outline-none focus:outline-none focus:ring w-full pl-10"
+                    />
+                </div>
+            </div>
+        </nav>
     )
 }
 
 export default Navbar
+
--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,13 @@
 import '../styles/globals.css';
 import Navbar from '../components/Navbar';
+import Sidebar from '../components/Sidebar';
+
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
       <Navbar />
+      <Sidebar />
       <Component {...pageProps} />
     </>
   )
